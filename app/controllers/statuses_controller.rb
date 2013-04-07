c0c4015c06e34219a43c19f34afcdbc6fe7b47e1
Bryan Hurren <bryan.hurren@gmail.com>
2013-04-07 16:12:13 -0700

    trying to establish an association8

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -40,7 +40,7 @@ class StatusesController < ApplicationController
   # POST /statuses
   # POST /statuses.json
   def create
-    @status = Status.new(params[:status])
+    @status = current_user.statuses.build(params[:status])
 
     respond_to do |format|
       if @status.save
