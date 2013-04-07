3a797e0f48354fbf9f05315dcc7df067b4202f5a
Bryan Hurren <bryan.hurren@gmail.com>
2013-04-06 23:04:25 -0700

    reinitialized files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bricks::Application.config.secret_token = '51c8da3cc7fab94bfb9ecc2292e6742c2be6f2d4a342fce247dcb0f4502c797698b19151968e8bab59f5e1ffbca49718b9e0a5ef3b936f0bfca82802ef3864eb'
