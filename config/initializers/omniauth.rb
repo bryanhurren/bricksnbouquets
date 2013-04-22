3dec4f8415d94177ca3f1e3d88dfdbc28087aa00
Bryan Hurren <bryan.hurren@gmail.com>
2013-04-21 21:12:39 -0700

    reverted env varabiles to isolate issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, '505706086152635', '6d230c88b34a5076bcc60c8e83ac0614',
            :scope => 'email,publish_actions', :display => 'popup'
 end
